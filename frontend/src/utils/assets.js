e21d3fd89083b1b85538f22904163041ccf662bd
8amjoker404 <8amjoker@gmail.com>
2026-07-23 16:17:16 -0700

    files update

--- frontend/src/utils/assets.js
+++
@@ -1,7 +0,0 @@
-import { API_ORIGIN } from "../api/axios";
-
-export function resolveAsset(url) {
-  if (!url) return "";
-  if (url.startsWith("http")) return url;
-  return `${API_ORIGIN}${url}`;
-}
